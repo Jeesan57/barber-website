748ccf5e5e61f623d2859184c346d24f7f1f336e
greyonion404 <twitterpicker111@gmail.com>
2022-11-18 00:26:13 +0600

    updated

--- JS/client/edit-shop.js
+++ JS/client/edit-shop.js
@@ -76,6 +76,95 @@ async function loadPage() {
     contact.textContent = shop.contactInformation;
     workingHours.textContent = shop.workingHours;
 
+    const container = document.getElementById('container');
+    // remove everything from container
+    // while (container.firstChild) {
+    //     container.removeChild(container.lastChild);
+    // }
+
+
+    const category = document.createElement('div');
+    const categoryInformation = document.createElement('div');
+
+    const info = document.createElement('div');
+    const categoryTitle = document.createElement('h3');
+    const categoryName = document.createElement('p');
+    categoryName.contentEditable = "true";
+
+    const save = document.createElement('button');
+    const remove = document.createElement('button');
+
+
+    categoryTitle.textContent = "Chategory Name: ";
+    categoryName.textContent = "dynamic texts";
+    save.textContent = "Save Category Name";
+    remove.textContent = "remove Category";
+
+
+
+
+    category.classList.add('chategory');
+    categoryInformation.classList.add('chategory-information');
+
+    info.classList.add('info-box');
+    categoryName.classList.add('chategory-current-name');
+    save.classList.add('save-chategory-button');
+    remove.classList.add('remove-chategory-button');
+
+
+    info.appendChild(categoryTitle);
+    info.appendChild(categoryName);
+    categoryInformation.appendChild(info);
+
+    categoryInformation.appendChild(save);
+    categoryInformation.appendChild(remove);
+    category.appendChild(categoryInformation);
+
+    container.appendChild(category);
+
+
+    // <div class="chategory">
+    //     <div class="chategory-information">
+    //         <div class="info-box">
+    //             <h3>Chategory Name: </h3>
+    //             <p class="chategory-current-name" contenteditable="true">chatergory name</p>
+    //         </div>
+
+    //         <button class="save-chategory-button">save chategory name</button>
+    //         <button class="remove-chategory-button">remove chategory</button>
+    //     </div>
+
+
+
+    //     <div class="service-adder-div">
+    //         <input type="text" class="service-info" placeholder="service name">
+    //             <input type="text" class="service-info" placeholder="service price">
+    //                 <button class="add-chategory-button">add service <i class="fa-sharp fa-solid fa-add"></i>
+    //                 </button>
+    //             </div>
+
+
+    //             <div class="service">
+    //                 <div class="service-information">
+    //                     <div class="info-box">
+    //                         <h3>Service Name: </h3>
+    //                         <p class="service-name" contenteditable="true">service name </p>
+    //                     </div>
+    //                     <div class="info-box">
+    //                         <h3>Price: </h3>
+
+    //                         <p class="service-price" contenteditable="true">30</p>
+    //                         <p class="currency-text">$</p>
+    //                     </div>
+    //                 </div>
+    //                 <div>
+    //                     <button class="save-service-button">save changes</button>
+    //                     <button class="remove-service-button">remove service</button>
+    //                 </div>
+    //             </div>
+
+    //     </div>
+
 
 }
 
@@ -99,12 +188,11 @@ async function updateShop() {
 }
 
 
-async function addCategory()
-{
+async function addCategory() {
     // http://localhost:3000/add-category?categoryID=123&categoryName=cut-hair&shopID=123
 
     let value = document.getElementById('category-name').value;
-    if(!value || value === "") return;
+    if (!value || value === "") return;
 
     let shopID = getQueryParams()?.shopID;
     let categoryID = getRandomString("CATEGORY");
